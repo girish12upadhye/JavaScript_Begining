3facb2c9ca76186b68e422c171786539867b2d01
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-07 12:57:01 +0000

    Learning datatypes in JS

---
+++ 01_Session/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("Datatypes in JavaScript")
+
+// alert( 4 * 5) // we are using nodejs, not browser
+
+console.log(4 + 6); console.log("Girish") // code readablility should be high
+
+let userName = "girish";
+let age = 15;
+let isLoggedIn = false;
+let state;
+let student = null;
+
+console.log(typeof userName)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof student)
