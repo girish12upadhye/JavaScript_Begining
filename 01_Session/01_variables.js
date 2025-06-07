1f9f2d25cae9491f301284318913c2f0dd755542
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-07 11:26:22 +0000

    learning variables in JS

---
+++ 01_Session/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1442332
+let accountEmail = "girish.g.u@email.com"
+var accountPassword = "upadhye@2319"
+accountCity = "Akola"
+let accountState;
+var accountName;
+// accountId = 2933
+accountCity = "Mangrulpir"
+
+/*
+Prefer not use var
+because of issue in block and functional scope
+ */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountName])
