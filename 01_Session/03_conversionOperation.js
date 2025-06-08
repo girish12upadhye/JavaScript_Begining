02afe53c97a3e7aa7316496082dae26f5547bd63
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-08 03:54:15 +0000

    modified 03_concersionoperation

--- 01_Session/03_conversionOperation.js
+++ 01_Session/03_conversionOperation.js
@@ -1,11 +1,10 @@
 let score = null
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(valueInNumber)
-console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,25 +12,61 @@ console.log(typeof valueInNumber)
 // undefined => Nan
 // null => 0
 
-console.log("=========================================")
+// ********************* Conversion Operation *********************
 
 let isLoggedIn = undefined
 
-console.log(typeof isLoggedIn)
-console.log(isLoggedIn)
+// console.log(typeof isLoggedIn)
+// console.log(isLoggedIn)
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // ''/"" => false; 'abc'/"abc" => true
 // null => false
 // undefined => false
 
-console.log("=========================================")
+// ********************* Conversion Operation *********************
+
 let someNumber = 565
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Conversion Operation *********************
+let value = 56
+
+let negative = -value
+// console.log(negative)
+// console.log(typeof negative)
+
+let str1 = "Hello"
+let str2 = " Girish"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+// console.log(+true);
+// console.log(true+) // bad practice
+// console.log(+"");
+
+
+// console.log(+false);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1);
+
