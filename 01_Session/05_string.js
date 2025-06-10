c8e9d0eb173fddf4b8353253d175e03c33f48958
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-10 14:00:49 +0000

    Learning String for JS

---
+++ 01_Session/05_string.js
@@ -0,0 +1,13 @@
+let userName = "Welcome to TechUniverse"
+let repoCount = 54
+
+// console.log(userName +", Total repo count for our channel is "+repoCount);
+
+console.log(`Our Channel name is ${userName.toUpperCase()}, total repo count for our channel is ${repoCount}`);
+
+let address = new String('Kajaleshwar')
+console.log(address[4])
+console.log(address.length)
+
+console.log(userName[11])
+
