eac752835d4ba1bc8bfc36adc49b4a71e0ce9688
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-11 14:26:42 +0000

    Learning Arrays in JS

---
+++ 02_Section/01_Arrays.js
@@ -0,0 +1,38 @@
+// let arr1 = [2, 3, 4, 1, 9];
+// let myArr = ["Girish", "Tanaya", "Vaibhav"]
+
+// let numArr = new Array(1, 5, 2, 9, 3, 7, 6);
+// console.log(arr1);
+// console.log(numArr);
+
+// arr1.push("Giri")
+// arr1.pop()
+// arr1.unshift(10)
+// arr1.shift()
+
+// console.log(arr1.includes(9));
+
+// let updatedArr = arr1.join()
+// console.log(arr1);
+// console.log(typeof updatedArr);
+
+let myn0 = [0, 1, 2, 3, 4, 5]
+
+// slice, splice
+
+console.log("A", myn0);
+
+let myn1 = myn0.slice(1,3)
+
+console.log(myn1);
+console.log("B", myn0);
+
+let myn2 = myn0.splice(1,3)
+
+console.log(myn2);
+console.log("C", myn0);
+
+
+
+
+
