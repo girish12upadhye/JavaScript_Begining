4000791562616c52fc2da33bb1baea6c8944412b
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-22 09:41:13 +0000

    Learning for in loop in JS

---
+++ 05_Section_iteration/four.js
@@ -0,0 +1,29 @@
+// for in
+
+const user = {
+    name : "girish",
+    email : "girish_g@walmart.com",
+    mobNo : 9623194356
+}
+
+// for (const key in user) {
+//     console.log(`${key} is ${user[key]}`);
+// }
+
+const programming = ['js', 'py', 'rb', 'java', 'cpp'];
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+// const map = new Map();
+// map.set('MH', 'Maharashtra')
+// map.set('ZH', 'Zarkhand')
+// map.set('KA', 'Karnataka')
+// map.set('MP', 'Madhy Pradesh')
+// map.set('HR', 'Hariyana')
+// map.set('PB', 'Punjab')
+
+// for (const key in map) {
+//     console.log(key);
+// }
