57e9838c5a385e592671354997c37a7a80651889
girish12upadhye <121550482+girish12upadhye@users.noreply.github.com>
2025-06-22 09:06:08 +0000

    Learning while, do while loops in JS

---
+++ 05_Section_iteration/two.js
@@ -0,0 +1,13 @@
+// while
+let names = ['girish', 'radhika', 'tushar', 'chetan', 'nutan'];
+
+let index = 0;
+
+//  
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
